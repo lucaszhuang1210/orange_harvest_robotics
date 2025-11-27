92aef5f262f1a5ad79f8ffd2e6e753d0e8e1fef5
Lucas Zhuang <lucaszhuang1210@gmail.com>
2025-11-26 17:11:33 -0800

    register task Template-Citrus-Cutting-Direct-v0

--- source/orange_harvest_robotics/orange_harvest_robotics/tasks/direct/orange_harvest_robotics/__init__.py
+++ source/orange_harvest_robotics/orange_harvest_robotics/tasks/direct/orange_harvest_robotics/__init__.py
@@ -21,3 +21,15 @@ gym.register(
         "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:PPORunnerCfg",
     },
 )
+
+gym.register(
+    id="Template-Citrus-Cutting-Direct-v0",
+    entry_point=f"{__name__}.citrus_cutting_env:CitrusCuttingEnv",
+    disable_env_checker=True,
+    kwargs={
+        "env_cfg_entry_point": f"{__name__}.citrus_cutting_env:CitrusCuttingEnvCfg",
+        "rl_games_cfg_entry_point": f"{agents.__name__}:rl_games_ppo_cfg.yaml",
+        "rsl_rl_cfg_entry_point": f"{agents.__name__}.rsl_rl_ppo_cfg:CitrusCuttingPPORunnerCfg",
+        "skrl_cfg_entry_point": f"{agents.__name__}:skrl_ppo_cfg.yaml",
+    },
+)
